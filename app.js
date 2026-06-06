06aa4ae1e611f163c3d4550cdaa98a421cd058e4
Tokky0117 <kaztok.9840@gmail.com>
2026-06-06 18:40:51 +0900

    app.js を更新

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ========================================
 // 基本設定
 // ========================================
-const APP_VERSION = "2.1.7";
+const APP_VERSION = "2.1.8";
 const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzVXg3onyOQzhidikArLr1gRc0L1Px3oNK5fQs6VqNA3XoxLJ_y4I35GEmofCB2g7Cn7g/exec";
 
 const SAVE_PAYLOAD_WARNING_LENGTH = 6000;
